31911df5f9a0f136150f3e19051865f62c777d56
VindiPerera <windypereira39@gmail.com>
2025-12-18 12:58:18 +0530

    operator enable to print bar code option

--- resources/js/Components/custom/ProductViewModel.vue
+++ resources/js/Components/custom/ProductViewModel.vue
@@ -199,7 +199,7 @@
                     />
 
                     <button
-                      v-if="HasRole(['Admin', 'Manager'])"
+                      v-if="HasRole(['Admin', 'Manager', 'Operator'])"
                       class="w-full px-4 py-3 text-2xl font-semibold tracking-widest text-white bg-blue-600 rounded-xl hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-400 focus:ring-offset-2"
                       @click="generateAndPrintBarcode"
                     >
